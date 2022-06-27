0426f98521f9b471106b7e06a96484c50f1e8b2c
Daniel Schmitz <danieljfa@gmail.com>
2022-06-27 08:27:02 -0300

    Updates

---
+++ precache-manifest.4c862a12acd253d71ecd5cf1dd87e07a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7eb6e401a9a3444d7b3c",
+    "url": "/react-mui-router/static/css/main.d1c40e1b.chunk.css"
+  },
+  {
+    "revision": "7eb6e401a9a3444d7b3c",
+    "url": "/react-mui-router/static/js/main.7eb6e401.chunk.js"
+  },
+  {
+    "revision": "144f52987a19d08cff73",
+    "url": "/react-mui-router/static/js/1.144f5298.chunk.js"
+  },
+  {
+    "revision": "c7f8236338ab3012b151",
+    "url": "/react-mui-router/static/js/runtime~main.c7f82363.js"
+  },
+  {
+    "revision": "77640cf2a5a21a775cbaf0614ba32d3d",
+    "url": "/react-mui-router/index.html"
+  }
+];
